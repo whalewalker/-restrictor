db8c8718dcf34ed00158468df955dd0ae2c7530f
whalewalker <mongodbms@gmail.com>
2023-05-15 16:08:48 +0100

    RL-8: Add an option to configure the blocking mechanism, such as the duration of the block or the number of violations required to trigger the block.

--- src/test/java/com/ratelimiter/service/TokenBucketRateLimiterTest.java
+++ src/test/java/com/ratelimiter/service/TokenBucketRateLimiterTest.java
@@ -1,15 +1,24 @@
 package com.ratelimiter.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class TokenBucketRateLimiterTest {
+    private TokenBucketRateLimiter rateLimiter;
+
+
+    @BeforeEach
+    public void setUp() {
+        // Initialize the rate limiter with appropriate values
+        rateLimiter = new TokenBucketRateLimiter(1000, 100, 60000, 10);
+    }
 
     @Test
      void testAllowMethod() {
         // Test case 1: Allow requests if enough tokens are available
-        TokenBucketRateLimiter rateLimiter = new TokenBucketRateLimiter(2, 1);
+         rateLimiter = new TokenBucketRateLimiter(2, 1);
         boolean allowRequest = rateLimiter.allow("client1");
         assertTrue(allowRequest);
 
@@ -24,7 +33,7 @@ class TokenBucketRateLimiterTest {
     @Test
      void testTokenRefill() throws InterruptedException {
         // Test case 1: Refill tokens after a delay
-        TokenBucketRateLimiter rateLimiter = new TokenBucketRateLimiter(2, 1);
+         rateLimiter = new TokenBucketRateLimiter(2, 1);
         rateLimiter.allow("client1");
         rateLimiter.allow("client1");
         boolean allowRequest = rateLimiter.allow("client1");
@@ -49,7 +58,7 @@ class TokenBucketRateLimiterTest {
    @Test
     void testGetRemainingTimeMillis_EnoughTokens_ReturnsZero() {
       // Arrange
-      TokenBucketRateLimiter rateLimiter = new TokenBucketRateLimiter(10, 1);
+       rateLimiter = new TokenBucketRateLimiter(10, 1);
 
       // Act
       long remainingTimeMillis = rateLimiter.getRemainingTimeMillis("client1");
@@ -61,7 +70,7 @@ class TokenBucketRateLimiterTest {
    @Test
     void testGetRemainingTimeMillis_TokensExhausted_ReturnsCorrectTime() {
       // Arrange
-      TokenBucketRateLimiter rateLimiter = new TokenBucketRateLimiter(2, 1);
+       rateLimiter = new TokenBucketRateLimiter(2, 1);
       rateLimiter.allow("client1");
       rateLimiter.allow("client1");
 
@@ -85,6 +94,47 @@ class TokenBucketRateLimiterTest {
       assertEquals(Long.MAX_VALUE, remainingTimeMillis, "Remaining time should be Long.MAX_VALUE");
    }
 
+    @Test
 
+     void testIsBlocked() {
+        String clientId = "test-client";
+        long currentTime = System.currentTimeMillis();
+
+        // Initially, the client should not be blocked
+        assertFalse(rateLimiter.isBlocked(clientId, currentTime));
+
+        // Block the client
+        rateLimiter.blockClient(clientId, currentTime);
+
+        // The client should be blocked
+        assertTrue(rateLimiter.isBlocked(clientId, currentTime));
+
+        // Advance time to after the block duration
+        currentTime += 60000;
+
+        // The client should not be blocked anymore
+        assertFalse(rateLimiter.isBlocked(clientId, currentTime));
+    }
+
+    @Test
+
+     void testIncrementViolationCount() {
+        String clientId = "test-client";
+
+        // Initially, the violation count should be 0
+        assertEquals(0, rateLimiter.getViolationCount(clientId));
+
+        // Increment the violation count
+        rateLimiter.incrementViolationCount(clientId);
+
+        // The violation count should be 1
+        assertEquals(1, rateLimiter.getViolationCount(clientId));
+
+        // Increment the violation count again
+        rateLimiter.incrementViolationCount(clientId);
+
+        // The violation count should be 2
+        assertEquals(2, rateLimiter.getViolationCount(clientId));
+    }
 
 }
